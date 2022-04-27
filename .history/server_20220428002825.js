7636f299c35688fd7af6cc2c498334b27a52beb7
Hyeonwoo <sksk2425@naver.com>
2022-04-28 00:33:48 +0900

    connect server.js css, javascript

---
+++ .history/server_20220428002825.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+
+require('dotenv').config()
+
+app.get('/', (req, res)=> {
+  res.render('index.ejs', {})
+})
+
+app.get('/html', (req, res)=> {
+  res.render('html.ejs', {})
+})
+
+app.get('/css', (req, res)=> {
+  
+})
+
+app.listen(process.env.PORT , function(){
+  console.log("listening on server");
+});
---
+++ .history/server_20220428002849.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+
+require('dotenv').config()
+
+app.get('/', (req, res)=> {
+  res.render('index.ejs', {})
+})
+
+app.get('/html', (req, res)=> {
+  res.render('html.ejs', {})
+})
+
+app.get('/css', (req, res)=> {
+  res.render('css.ejs', {})
+})
+
+app.get('/javascript', (req, res)=> {
+  
+})
+
+app.listen(process.env.PORT , function(){
+  console.log("listening on server");
+});
